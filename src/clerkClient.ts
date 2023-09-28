36826d83cd7cdd85923441122f2b385c9391c487
wobsoriano <sorianorobertc@gmail.com>
2023-09-28 10:05:04 -0700

    style: switch to biome

--- src/clerkClient.ts
+++ src/clerkClient.ts
@@ -1,12 +1,12 @@
-import { Clerk } from '@clerk/backend'
+import { Clerk } from '@clerk/backend';
 
-import { API_URL, API_VERSION, JWT_KEY, SECRET_KEY } from './constants'
+import { API_URL, API_VERSION, JWT_KEY, SECRET_KEY } from './constants';
 
-export const createClerkClient = Clerk
+export const createClerkClient = Clerk;
 
 export const clerkClient = createClerkClient({
-  secretKey: SECRET_KEY,
-  apiUrl: API_URL,
-  apiVersion: API_VERSION,
-  jwtKey: JWT_KEY,
-})
+	secretKey: SECRET_KEY,
+	apiUrl: API_URL,
+	apiVersion: API_VERSION,
+	jwtKey: JWT_KEY,
+});
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,61 @@
-import { Elysia } from 'elysia'
-import type { ClerkOptions, SignedInAuthObject, SignedOutAuthObject } from '@clerk/backend'
-import * as constants from './constants'
-import { clerkClient } from './clerkClient'
+import type {
+	ClerkOptions,
+	SignedInAuthObject,
+	SignedOutAuthObject,
+} from '@clerk/backend';
+import { Elysia } from 'elysia';
+import { clerkClient } from './clerkClient';
+import * as constants from './constants';
 
 function clerkPlugin(options?: ClerkOptions) {
-  const secretKey = options?.secretKey ?? constants.SECRET_KEY
-  const publishableKey = options?.publishableKey ?? constants.PUBLISHABLE_KEY
+	const secretKey = options?.secretKey ?? constants.SECRET_KEY;
+	const publishableKey = options?.publishableKey ?? constants.PUBLISHABLE_KEY;
 
-  return new Elysia({
-    name: 'clerk',
-    seed: options,
-  })
-    .decorate('clerk', clerkClient)
-    .state('auth', null as null | SignedInAuthObject | SignedOutAuthObject)
-    .onBeforeHandle(async ({ request, set, store }) => {
-      const requestState = await clerkClient.authenticateRequest({
-        ...options,
-        secretKey,
-        publishableKey,
-        apiKey: constants.API_KEY,
-        frontendApi: constants.FRONTEND_API,
-        request,
-      })
+	return new Elysia({
+		name: 'clerk',
+		seed: options,
+	})
+		.decorate('clerk', clerkClient)
+		.state('auth', null as null | SignedInAuthObject | SignedOutAuthObject)
+		.onBeforeHandle(async ({ request, set, store }) => {
+			const requestState = await clerkClient.authenticateRequest({
+				...options,
+				secretKey,
+				publishableKey,
+				apiKey: constants.API_KEY,
+				frontendApi: constants.FRONTEND_API,
+				request,
+			});
 
-      if (requestState.isUnknown) {
-        set.status = 401
-        set.headers = {
-          [constants.Headers.AuthReason]: requestState.reason,
-          [constants.Headers.AuthMessage]: requestState.message,
-        }
-        return ''
-      }
+			if (requestState.isUnknown) {
+				set.status = 401;
+				set.headers = {
+					[constants.Headers.AuthReason]: requestState.reason,
+					[constants.Headers.AuthMessage]: requestState.message,
+				};
+				return '';
+			}
 
-      if (requestState.isInterstitial) {
-        const interstitialHtmlPage = clerkClient.localInterstitial({
-          publishableKey,
-          frontendApi: constants.FRONTEND_API,
-        })
+			if (requestState.isInterstitial) {
+				const interstitialHtmlPage = clerkClient.localInterstitial({
+					publishableKey,
+					frontendApi: constants.FRONTEND_API,
+				});
 
-        set.status = 401
-        set.headers = {
-          [constants.Headers.AuthReason]: requestState.reason,
-          [constants.Headers.AuthMessage]: requestState.message,
-          'Content-Type': 'text/html',
-        }
+				set.status = 401;
+				set.headers = {
+					[constants.Headers.AuthReason]: requestState.reason,
+					[constants.Headers.AuthMessage]: requestState.message,
+					'Content-Type': 'text/html',
+				};
 
-        return interstitialHtmlPage
-      }
+				return interstitialHtmlPage;
+			}
 
-      store.auth = requestState.toAuth()
-    })
+			store.auth = requestState.toAuth();
+		});
 }
 
-export {
-  clerkPlugin,
-  type ClerkOptions,
-}
+export { clerkPlugin, type ClerkOptions };
 
-export { clerkClient, createClerkClient } from './clerkClient'
+export { clerkClient, createClerkClient } from './clerkClient';
