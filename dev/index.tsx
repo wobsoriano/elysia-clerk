f8c8c70dde3a265a682a141a2400bd52901f3dbe
wobsoriano <sorianorobertc@gmail.com>
2023-10-20 23:41:43 -0700

    tmp type fix

--- dev/index.tsx
+++ dev/index.tsx
@@ -15,6 +15,7 @@ const subset = new Elysia({ prefix: '/api' })
   })
 
 const app = new Elysia()
+  // @ts-expect-error: Incorrect html plugin types
   .use(html())
   .use(clerkPlugin({}))
   // .get('/', ({ html }) => html(contents))
