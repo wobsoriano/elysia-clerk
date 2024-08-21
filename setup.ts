d38e6eff9b8980f2e57187c674115c2498c0e9ae
wobsoriano <sorianorobertc@gmail.com>
2024-08-21 16:27:09 -0700

    docs: update readme

--- setup.ts
+++ setup.ts
@@ -4,8 +4,6 @@ import { mock, jest } from 'bun:test'
 globalThis.PACKAGE_NAME = pkg.name
 globalThis.PACKAGE_VERSION = pkg.version
 
-console.log(import.meta.env.TESTS)
-
 process.env.CLERK_SECRET_KEY = 'TEST_SECRET_KEY'
 process.env.CLERK_PUBLISHABLE_KEY = 'pk_test_dGlnaHQtcmFiYml0LTk0LmNsZXJrLmFjY291bnRzLmRldiQ'
 
