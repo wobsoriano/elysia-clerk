8992f2421199539e8f0f3d0114ce33ed957e1478
wobsoriano <sorianorobertc@gmail.com>
2024-09-20 12:05:51 -0700

    chore: Disable handshake by default

--- src/plugin.ts
+++ src/plugin.ts
@@ -4,9 +4,22 @@ import { Elysia } from 'elysia';
 import { clerkClient } from './clerkClient';
 import * as constants from './constants';
 
-export function clerkPlugin(options?: ClerkOptions) {
+export type ElysiaClerkOptions = ClerkOptions & {
+	/**
+	 * Enables Clerk's handshake flow, which helps verify the session state
+	 * when a session JWT has expired. It issues a 307 redirect to refresh
+	 * the session JWT if the user is still logged in.
+	 *
+	 * This is useful for server-rendered fullstack applications to handle
+	 * expired JWTs securely and maintain session continuity.
+	 */
+	enableHandshake?: boolean;
+};
+
+export function clerkPlugin(options?: ElysiaClerkOptions) {
 	const secretKey = options?.secretKey ?? constants.SECRET_KEY;
 	const publishableKey = options?.publishableKey ?? constants.PUBLISHABLE_KEY;
+	const enableHandshake = options?.enableHandshake ?? false;
 
 	return new Elysia({
 		name: 'elysia-clerk',
@@ -20,25 +33,27 @@ export function clerkPlugin(options?: ClerkOptions) {
 				publishableKey,
 			});
 
-			requestState.headers.forEach((value, key) => {
-				set.headers[key] = value;
-			});
-
 			const auth = requestState.toAuth();
 
-			const locationHeader = requestState.headers.get(
-				constants.Headers.Location,
-			);
-			if (locationHeader) {
-				// Trigger a handshake redirect
-				set.status = 307;
-				return {
-					auth,
-				};
-			}
+			if (enableHandshake) {
+				requestState.headers.forEach((value, key) => {
+					set.headers[key] = value;
+				});
 
-			if (requestState.status === AuthStatus.Handshake) {
-				throw new Error('Clerk: handshake status without redirect');
+				const locationHeader = requestState.headers.get(
+					constants.Headers.Location,
+				);
+				if (locationHeader) {
+					// Trigger a handshake redirect
+					set.status = 307;
+					return {
+						auth,
+					};
+				}
+
+				if (requestState.status === AuthStatus.Handshake) {
+					throw new Error('Clerk: handshake status without redirect');
+				}
 			}
 
 			return {
