be3fe0547cb46b15fd51c58a5df87d67285e13f1
Torben Köhler <torben@sonic.tech>
2025-04-24 09:09:03 +0200

    accept functions to work in a wrangler setup

--- src/plugin.ts
+++ src/plugin.ts
@@ -4,14 +4,17 @@ import { Elysia } from 'elysia';
 import { clerkClient } from './clerkClient';
 import * as constants from './constants';
 
-export type ElysiaClerkOptions = ClerkOptions;
+export type ElysiaClerkOptions = Omit<ClerkOptions, "publishableKey" | "secretKey"> & {
+	secretKey: ClerkOptions["secretKey"] | (() => string)
+	publishableKey: ClerkOptions["publishableKey"] | (() => string)
+};
 
 const HandshakeStatus = 'handshake';
 const LocationHeader = 'location';
 
 export function clerkPlugin(options?: ElysiaClerkOptions) {
-	const secretKey = options?.secretKey ?? constants.SECRET_KEY;
-	const publishableKey = options?.publishableKey ?? constants.PUBLISHABLE_KEY;
+	const rawSecretKey = options?.secretKey ?? constants.SECRET_KEY;
+	const rawPublishableKey = options?.publishableKey ?? constants.PUBLISHABLE_KEY;
 
 	return new Elysia({
 		name: 'elysia-clerk',
@@ -19,6 +22,8 @@ export function clerkPlugin(options?: ElysiaClerkOptions) {
 	})
 		.decorate('clerk', clerkClient)
 		.resolve(async ({ request, set }) => {
+			const secretKey = typeof rawSecretKey === 'function' ? rawSecretKey() : rawSecretKey;
+			const publishableKey = typeof rawPublishableKey === 'function' ? rawPublishableKey() : rawPublishableKey;
 			const requestState = await clerkClient.authenticateRequest(request, {
 				...options,
 				secretKey,
