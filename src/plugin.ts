653f1cb471ca0f90a142c27c55c504e8a3700454
wobsoriano <sorianorobertc@gmail.com>
2025-05-05 08:48:39 -0700

    chore: switch bundler to tsdown

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,11 +3,14 @@ import { deprecated } from '@clerk/shared/deprecated';
 import { Elysia } from 'elysia';
 import { clerkClient } from './clerkClient';
 import * as constants from './constants';
+import type { SignedInAuthObject, SignedOutAuthObject } from '@clerk/backend/internal';
 
 export type ElysiaClerkOptions = ClerkOptions;
 
 const HandshakeStatus = 'handshake';
 const LocationHeader = 'location';
+
+type AuthObject = SignedInAuthObject | SignedOutAuthObject
 
 export function clerkPlugin(options?: ElysiaClerkOptions) {
 	const secretKey = options?.secretKey ?? constants.SECRET_KEY;
@@ -25,7 +28,8 @@ export function clerkPlugin(options?: ElysiaClerkOptions) {
 				publishableKey,
 			});
 
-			const authObject = requestState.toAuth();
+			// Asserting to fix error TS2742 when building
+			const authObject = requestState.toAuth() as AuthObject;
 			const authHandler = () => authObject;
 
 			const auth = new Proxy(Object.assign(authHandler, authObject), {
