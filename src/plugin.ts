0126fcda884262094972b326e9b47b471944070a
wobsoriano <sorianorobertc@gmail.com>
2025-07-09 08:54:39 -0700

    chore: run format

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,7 +3,7 @@ import type {
 	SignedOutAuthObject,
 } from '@clerk/backend/internal';
 import { TokenType } from '@clerk/backend/internal';
-import type { ClerkOptions } from '@clerk/backend'
+import type { ClerkOptions } from '@clerk/backend';
 import type { PendingSessionOptions } from '@clerk/types';
 import { Elysia } from 'elysia';
 import { clerkClient } from './clerkClient';
@@ -20,41 +20,42 @@ export function clerkPlugin(options?: ClerkOptions) {
 	const secretKey = options?.secretKey ?? constants.SECRET_KEY;
 	const publishableKey = options?.publishableKey ?? constants.PUBLISHABLE_KEY;
 
-  return new Elysia({
-    name: 'elysia-clerk',
-    seed: options,
-  })
-    .decorate('clerk', clerkClient)
-    .resolve(async ({ request, set }) => {
-      const requestState = await clerkClient.authenticateRequest(request, {
-        ...options,
-        secretKey,
-        publishableKey,
-        acceptsToken: TokenType.SessionToken,
-      });
+	return new Elysia({
+		name: 'elysia-clerk',
+		seed: options,
+	})
+		.decorate('clerk', clerkClient)
+		.resolve(async ({ request, set }) => {
+			const requestState = await clerkClient.authenticateRequest(request, {
+				...options,
+				secretKey,
+				publishableKey,
+				acceptsToken: TokenType.SessionToken,
+			});
 
-      const auth = (options?: PendingSessionOptions) =>
-        requestState.toAuth(options) as SessionAuthObject;
+			const auth = (options?: PendingSessionOptions) =>
+				requestState.toAuth(options) as SessionAuthObject;
 
-      requestState.headers.forEach((value, key) => {
-        set.headers[key] = value;
-      });
+			requestState.headers.forEach((value, key) => {
+				set.headers[key] = value;
+			});
 
-      const locationHeader = requestState.headers.get(LocationHeader);
-      if (locationHeader) {
-        // Trigger a handshake redirect
-        set.status = 307;
-        return {
-          auth,
-        };
-      }
+			const locationHeader = requestState.headers.get(LocationHeader);
+			if (locationHeader) {
+				// Trigger a handshake redirect
+				set.status = 307;
+				return {
+					auth,
+				};
+			}
 
-      if (requestState.status === HandshakeStatus) {
-        throw new Error('Clerk: handshake status without redirect');
-      }
+			if (requestState.status === HandshakeStatus) {
+				throw new Error('Clerk: handshake status without redirect');
+			}
 
-      return {
-        auth,
-      };
-    }).as('scoped');
+			return {
+				auth,
+			};
+		})
+		.as('scoped');
 }
