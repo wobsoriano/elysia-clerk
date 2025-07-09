4a329d69768d13ff0c8fadd2c09823491cb7457e
wobsoriano <sorianorobertc@gmail.com>
2025-07-09 08:45:21 -0700

    chore: remove deprecated scope

--- src/plugin.ts
+++ src/plugin.ts
@@ -20,42 +20,41 @@ export function clerkPlugin(options?: ClerkOptions) {
 	const secretKey = options?.secretKey ?? constants.SECRET_KEY;
 	const publishableKey = options?.publishableKey ?? constants.PUBLISHABLE_KEY;
 
-	return new Elysia({
-		name: 'elysia-clerk',
-		seed: options,
-	})
-		.decorate('clerk', clerkClient)
-		.resolve(async ({ request, set }) => {
-			const requestState = await clerkClient.authenticateRequest(request, {
-				...options,
-				secretKey,
-				publishableKey,
-				acceptsToken: TokenType.SessionToken,
-			});
+  return new Elysia({
+    name: 'elysia-clerk',
+    seed: options,
+  })
+    .decorate('clerk', clerkClient)
+    .resolve(async ({ request, set }) => {
+      const requestState = await clerkClient.authenticateRequest(request, {
+        ...options,
+        secretKey,
+        publishableKey,
+        acceptsToken: TokenType.SessionToken,
+      });
 
-			const auth = (options?: PendingSessionOptions) =>
-				requestState.toAuth(options) as SessionAuthObject;
+      const auth = (options?: PendingSessionOptions) =>
+        requestState.toAuth(options) as SessionAuthObject;
 
-			requestState.headers.forEach((value, key) => {
-				set.headers[key] = value;
-			});
+      requestState.headers.forEach((value, key) => {
+        set.headers[key] = value;
+      });
 
-			const locationHeader = requestState.headers.get(LocationHeader);
-			if (locationHeader) {
-				// Trigger a handshake redirect
-				set.status = 307;
-				return {
-					auth,
-				};
-			}
+      const locationHeader = requestState.headers.get(LocationHeader);
+      if (locationHeader) {
+        // Trigger a handshake redirect
+        set.status = 307;
+        return {
+          auth,
+        };
+      }
 
-			if (requestState.status === HandshakeStatus) {
-				throw new Error('Clerk: handshake status without redirect');
-			}
+      if (requestState.status === HandshakeStatus) {
+        throw new Error('Clerk: handshake status without redirect');
+      }
 
-			return {
-				auth,
-			};
-		})
-		.as('plugin');
+      return {
+        auth,
+      };
+    });
 }
