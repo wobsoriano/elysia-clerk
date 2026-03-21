ed1d75adc19fc2de4a8b457fe1d45b80a1eee775
wobsoriano <sorianorobertc@gmail.com>
2026-03-21 08:45:44 -0700

    feat: Upgrade to Clerk Core 3

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,6 @@ import pkg from './package.json' with { type: 'json' }
 export default defineConfig(() => {
   return {
     entry: ['src/index.ts', 'src/webhooks.ts'],
-    platform: 'neutral',
     define: {
       PACKAGE_NAME: `"${pkg.name}"`,
       PACKAGE_VERSION: `"${pkg.version}"`,
